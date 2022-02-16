fa3cce48178e893e010c3a1328ea34e50da745fe
Gurjeet Matharu <gurjeet@navaventures.io>
2022-02-16 12:28:37 -0800

    Add todo_app schema.

---
+++ schema/deploy/appschema.sql
@@ -0,0 +1,7 @@
+-- Deploy todo_app:appschema to pg
+
+BEGIN;
+
+CREATE SCHEMA todo_app;
+
+COMMIT;
---
+++ schema/verify/appschema.sql
@@ -0,0 +1,10 @@
+-- Verify todo_app:appschema on pg
+
+BEGIN;
+
+DO $$
+    BEGIN
+        ASSERT (SELECT has_schema_privilege('todo_app', 'usage'));
+    END $$;
+
+ROLLBACK;
