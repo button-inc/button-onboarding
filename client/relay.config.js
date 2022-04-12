a8d6e7173ea1c67f0e493562c9c8efbd47187be2
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-04-12 14:30:59 -0700

    Tweak on packages and configs

--- client/relay.config.js
+++ client/relay.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // ...
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
-  language: "typescript",
   src: "./src",
   schema: "../schema/schema.graphql",
+  language: "typescript",
   exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
 };
