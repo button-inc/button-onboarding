15898688eb0fb07ee5544967a6a86e2249f6db19
tmastro <thomas.mastromonaco@gmail.com>
2022-08-11 13:50:20 -0700

    todos table

---
+++ schema/deploy/todos.sql
@@ -0,0 +1,16 @@
+-- Deploy todo:todos to pg
+-- requires: appschema
+
+BEGIN;
+
+SET client_min_messages = 'warning';
+
+CREATE TABLE todo.todos (
+	id serial PRIMARY KEY,
+	task VARCHAR (50) NOT NULL,
+	completed BOOLEAN NOT NULL, 
+	date_created TIMESTAMP NOT NULL, 
+	date_updated TIMESTAMP
+); 
+
+COMMIT;
