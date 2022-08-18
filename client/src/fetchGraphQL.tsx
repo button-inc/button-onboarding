2d2f0fecc163d7d38717528897f00d5cc3a6a22a
tmastro <thomas.mastromonaco@gmail.com>
2022-08-18 13:22:37 -0700

    set up relay with graphql example

---
+++ client/src/fetchGraphQL.tsx
@@ -0,0 +1,23 @@
+async function fetchGraphQL(text: string, variables:any) {
+    const REACT_APP_GITHUB_AUTH_TOKEN = process.env.REACT_APP_GITHUB_AUTH_TOKEN;
+  
+    // Fetch data from GitHub's GraphQL API:
+    const response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: {
+        Authorization: `bearer ${REACT_APP_GITHUB_AUTH_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: text,
+        variables,
+      }),
+    });
+
+    console.log(response);
+  
+    // Get the response as JSON
+    return await response.json();
+  }
+  
+  export default fetchGraphQL;
