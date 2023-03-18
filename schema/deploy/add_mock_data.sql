5c73cba835c20dbf2bc73fc78f4b4bf6dcc6620e
YaokunLin <yaokun1@ualberta.ca>
2023-03-18 14:53:07 -0700

    added more tasks

--- schema/deploy/add_mock_data.sql
+++ schema/deploy/add_mock_data.sql
@@ -11,6 +11,8 @@ BEGIN
     SELECT user_uid INTO v_user_id FROM todo_schema.users WHERE email = 'patrick@button.is' AND password = crypt('password', password);
 
     PERFORM todo_schema.insert_todo(v_user_id, 'walk the dog', false, '2023-01-01 09:00:00', '2023-01-01 09:00:00');
+    PERFORM todo_schema.insert_todo(v_user_id, 'wash disches', true,  '2023-01-01 08:00:00', '2023-01-01 09:00:00');
+    PERFORM todo_schema.insert_todo(v_user_id, 'gardening',    false, '2023-01-01 08:00:00', '2023-01-01 08:00:00');
 
 END $$;
 
